00423e0551252f85346af1b5def7dea2b73c6bd3
mssoham <gsoham.51@gmail.com>
2016-07-04 14:24:15 +0530

    4/7/16 2:24pm

--- DirectHiring.com/app/src/main/java/utilities/imageDownloadOperations/CategoryImageLoader.java
+++ DirectHiring.com/app/src/main/java/utilities/imageDownloadOperations/CategoryImageLoader.java
@@ -23,7 +23,7 @@ import java.util.WeakHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import com.example.re.R;
+import afroradix.xigmapro.com.directhiringcom.R;
 
 
 public class CategoryImageLoader {
